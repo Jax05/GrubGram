6d1271ba9d235f78f671e328e0e931f860c6a290
Jax05 <Jax05@users.noreply.github.com>
2021-04-19 21:09:45 -0500

    add routes for sign up and logging in/out

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   resources :photos
   resources :restaurants
-  resources :users
+  resources :users, except: [:new]
+
+  # custom route for signing up
+  get '/signup', to: 'users#new'
+
+  # custom routes for logging in/out
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
