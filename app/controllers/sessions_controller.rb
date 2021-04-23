2757330e8c1e530ce036f9e749b7793bf9e65e2b
Jax05 <Jax05@users.noreply.github.com>
2021-04-22 21:18:05 -0500

    Add create method logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
-    def new
+    def create
+        user = User.find_by(email: params[:user][:email])
+
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            redirect_to login_path
+        end
     end
 
     def destroy
