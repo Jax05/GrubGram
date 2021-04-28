b6d7c17f25b93e86ee78444beb404372de7dbc5d
Jax05 <Jax05@users.noreply.github.com>
2021-04-27 19:30:20 -0500

    Add omniauth action and logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,4 +15,19 @@ class SessionsController < ApplicationController
         redirect_to "/"
     end
 
+    def omniauth
+        user = User.find_or_create_by(uid: request.env['omniauth.auth'][:uid]) do |u|
+            u.username = request.env['omniauth.auth'][:info][:first_name]
+            u.email = request.env['omniauth.auth'][:info][:email]
+            u.password = SecureRandom.hex(15)
+        end
+
+        if user.valid?
+            session[:user_id] = user.id
+            redirect_to user_path(user)
+        else
+            redirect_to login_path
+        end
+    end
+
 end
