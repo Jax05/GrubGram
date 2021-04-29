3fb88b7e5cd2470e2be4cf9324e53837415211ac
Jax05 <Jax05@users.noreply.github.com>
2021-04-29 18:12:17 -0500

    Add basic photo actions and strong params

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,2 +1,29 @@
 class PhotosController < ApplicationController
+    def index
+        @photos = Photo.all
+    end
+
+    def new
+        @photo = Photo.new
+    end
+
+    def create
+        @photo = Photo.new(photo_params)
+
+        if @photo.save
+            redirect_to photo_path(@photo)
+        else
+            render :new
+        end
+    end
+
+    def show
+        @photo = Photo.find_by(id: params[:id])
+    end
+
+    private
+
+    def photo_params
+        params.require(:photo).permit(:url, :description, :rating)
+    end
 end
