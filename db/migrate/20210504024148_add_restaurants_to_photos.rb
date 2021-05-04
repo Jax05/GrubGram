eda4ddbd0873a4436252863894c2863a9186e697
Jax05 <Jax05@users.noreply.github.com>
2021-05-03 21:46:49 -0500

    Create restaurant table and add reference to photos

---
+++ db/migrate/20210504024148_add_restaurants_to_photos.rb
@@ -0,0 +1,5 @@
+class AddRestaurantsToPhotos < ActiveRecord::Migration[6.1]
+  def change
+    add_reference :photos, :restaurant, null: false, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_02_025924) do
+ActiveRecord::Schema.define(version: 2021_05_04_024148) do
 
   create_table "photos", force: :cascade do |t|
     t.string "url"
@@ -19,7 +19,16 @@ ActiveRecord::Schema.define(version: 2021_05_02_025924) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_photos_on_restaurant_id"
     t.index ["user_id"], name: "index_photos_on_user_id"
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "logo"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -31,5 +40,6 @@ ActiveRecord::Schema.define(version: 2021_05_02_025924) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "photos", "restaurants"
   add_foreign_key "photos", "users"
 end
