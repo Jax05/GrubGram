461b0efcbafea6bb4b28db02e6dbd2305e7b0c7a
Jax05 <Jax05@users.noreply.github.com>
2021-05-15 20:10:25 -0500

    Validate restaurant name is not blank

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -11,7 +11,7 @@ class Photo < ApplicationRecord
   # We use a custom writer instead of accepts_nested_attributes_for so we don't create duplicate restaurant instances
   # using the values submitted in our new photo form
   def restaurant_attributes=(restaurant_attributes)
-    if !restaurant_attributes.values.any? { |v| v.empty? }
+    if !restaurant_attributes["name"].blank?
       self.restaurant = Restaurant.find_or_create_by(restaurant_attributes)
     end
   end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    
     has_many :photos
     has_many :users, through: :photos
 end
