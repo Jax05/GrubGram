a6a4be0477886dcb3d93cde8e4b08ad257cdbb75
Jax05 <Jax05@users.noreply.github.com>
2021-05-15 19:32:58 -0500

    Add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
+    validates :username, uniqueness: true, allow_blank: true
+    validates :email, presence: true, uniqueness: true, format: { with: %r{^(([A-Za-z0-9]+_+)|([A-Za-z0-9]+\-+)|([A-Za-z0-9]+\.+)|([A-Za-z0-9]+\++))*[A-Za-z0-9]+@((\w+\-+)|(\w+\.))*\w{1,63}\.[a-zA-Z]{2,6}$}, message: "Email invalid", multiline: true }
     has_secure_password
+
     has_many :photos
     has_many :restaurants, through: :photos
 end
