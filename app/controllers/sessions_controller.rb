6bfcae44d6622d2f3a15e9f1dfe67073c75a168b
Jax05 <Jax05@users.noreply.github.com>
2021-05-15 20:21:27 -0500

    Create form partials for sessions and users

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,10 @@
 class SessionsController < ApplicationController
     def create
-        user = User.find_by(email: params[:user][:email])
+        @user = User.find_by(email: params[:user][:email])
 
-        if user && user.authenticate(params[:user][:password])
-            session[:user_id] = user.id
-            redirect_to user_path(user)
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
         else
             render :new
         end
