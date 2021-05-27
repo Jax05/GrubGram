cf72b0cbc9fdea31cf04e30c6e32e9eab386a435
Jax05 <Jax05@users.noreply.github.com>
2021-05-26 23:06:56 -0500

    Ensure failed logins flash an error message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,12 +1,12 @@
 class SessionsController < ApplicationController
     def create
-        @user = User.find_by(email: params[:user][:email])
+        user = User.find_by(email: params[:user][:email])
 
-        if @user && @user.authenticate(params[:user][:password])
-            session[:user_id] = @user.id
-            redirect_to user_path(@user)
+        if user && user.authenticate(params[:user][:password])
+            session[:user_id] = user.id
+            redirect_to user_path(user)
         else
-            render :new
+            redirect_to login_path, alert: "Login failed! Try again."
         end
     end
 
